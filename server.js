1a49c799b2b8f229aab4f3c15863586e5a59a8eb
Damian Marius <damian.mm@live.com>
2018-05-21 13:22:40 -0700

    added get notes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Load array of notes
-const data = require('./db/notes');
+// const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
@@ -9,14 +9,26 @@ console.log('Hello Noteful!');
 
 const express = require('express');
 
-// const data = require('./db/notes');
+const data = require('./db/notes');
 
 const app = express();
 
 // ADD STATIC SERVER HERE
+
+app.use(express.static('public'));
 
 app.listen(8080, function () {
     console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
     console.error(err);
 });
+
+// CREATE GET NOTES LIST ENDPOINT
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+  });
+
+app.get('/api/notes/:id', (req, res) => {
+    res.json(data);
+  });

73322163c0da2ff0103bc2217dca58935b54e464
Damian Marius <damian.mm@live.com>
2018-05-23 10:05:15 -0700

    updated through challenges 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+// EXPRESS APP CODE
 
 const express = require('express');
 
@@ -13,9 +13,20 @@ const data = require('./db/notes');
 
 const app = express();
 
-// ADD STATIC SERVER HERE
+// DB
+
+const { PORT } = require('./config');
+
+const {requestLogger} = require('./middleware/logger');
+
+const simDB = require('./db/simDB');
+
+const notes = simDB.initialize(data);
+
+// STATIC SERVER
 
 app.use(express.static('public'));
+app.use(requestLogger);
 
 app.listen(8080, function () {
     console.info(`Server listening on ${this.address().port}`);
@@ -23,12 +34,23 @@ app.listen(8080, function () {
     console.error(err);
 });
 
-// CREATE GET NOTES LIST ENDPOINT
+// SEARCH
 
-app.get('/api/notes', (req, res) => {
-    res.json(data);
+app.get('/api/notes', (req, res, next) => {
+    // res.json(data);
+    const { searchTerm } = req.query;
+    notes.filter(searchTerm, (err, list) => { 
+        if (err) {
+            return next(err); //to error handler
+        }
+        res.json(list); //array response
+    })
   });
 
-app.get('/api/notes/:id', (req, res) => {
-    res.json(data);
-  });
+
+
+// app.get('/api/notes/:id', (req, res) => {
+//     const foundItem = data.find(item => item.id == req.params.id);
+//     res.json(foundItem);
+//   });
+  
